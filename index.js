1bec0aff8a8e59e6cf20853b765c0be8bc902429
balanced-checkpoint-e039 <harmandeepk628@gmail.com>
2021-03-03 00:42:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function yourAnimal(animal) {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
-  animal !='dog'
-  animal === 'cat'
+  animal !='dog';
+  animal === 'cat';
   return animal
 
 }
