8d1f7cfcd427ee837ee88f924a43d722d80d4ab9
balanced-checkpoint-e039 <harmandeepk628@gmail.com>
2021-03-03 00:42:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function yourAnimal(animal) {
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
   animal !='dog';
-  animal === 'cat';
+  animal = 'cat';
   return animal
 
 }
